0332701cc315182f94c60b0f21c61e1a09ccadf7
erwan <erwan@DESKTOP-O25RGGR.home>
2023-01-17 22:02:28 +0100

    creation TP_0_Error

---
+++ src/main/java/fr/eni/tp_0_error/TP_0_Error.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.eni.tp_0_error;
+
+
+
+/**
+ *
+ * @author erwan
+ */
+public class TP_0_Error {
+
+    public static void main(String[] args) {
+        Int a = 18;
+        double b = 10;
+        int a = 10;
+        Scanner s = new Scanner();
+        System.out.println(b)
+        additionInt(a,b);
+        multiplicationInt(a,b);
+        int[] c = ["2","1","2","1"]
+    }
+    public static int addition(int a, int b)
+    {
+        return a + b;
+    }
+    
+    public static int multiplication(int b, double a)
+    {
+        int c = b * a; 
+    }
+}
